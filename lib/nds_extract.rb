508013a69c1fd12a0c96556cb7bec025f134ec8c
Will Brennan <willfbren@gmail.com>
2020-02-26 20:06:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,8 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   {
-    title => 
+    title => movie_data[:title],
+    
   }
 end
 
